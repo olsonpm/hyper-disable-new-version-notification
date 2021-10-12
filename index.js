05ef8a6fabc489683bfe1e9daceb479090475e70
Phil Olson <philip.olson@pm.me>
2021-10-12 11:42:18 -0500

    misc updates
    - fix bug where state.ui was immutable, so we needed to use the state
      methods to mutate it
    - update dev dependencies
    - format with updated deps

--- index.js
+++ index.js
@@ -18,24 +18,26 @@ let errorWasReported = false
 // Main //
 //------//
 
-const mapNotificationsState = state => {
+const mapNotificationsState = (state) => {
   try {
-    state.ui.notifications.updates = false
+    state.ui = state.ui.set(['noficiations', 'updates'], false)
   } catch (e) {
     if (errorWasReported) return
 
-    // eslint-disable-next-line no-console
-    console.error(
-      `This plugin 'hyper-disable-new-version-notification v${version}' expects` +
-        ' a state structure of "state.ui.notifications.updates".  It seems' +
-        ' either this structure has changed or the assumption is incorrect.' +
-        ' If this plugin is fully up to date then please file an issue at' +
-        ' https://github.com/olsonpm/hyper-disable-new-version-notification/issues/new.' +
-        `\n\n${e.stack}`
-    )
-
     errorWasReported = true
+
+    const msg =
+      `This plugin 'hyper-disable-new-version-notification v${version}' expects` +
+      ' a state structure of "state.ui.notifications.updates".  It seems' +
+      ' either this structure has changed or the assumption is incorrect.' +
+      ' If this plugin is fully up to date then please file an issue at' +
+      ' https://github.com/olsonpm/hyper-disable-new-version-notification/issues/new.' +
+      `\n\n${e.stack}`
+
+    // eslint-disable-next-line no-console
+    console.error(msg)
   }
+
   return state
 }
 
